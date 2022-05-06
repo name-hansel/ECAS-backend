dfc374a20f7a06b95e6115777f039005a1bbf7ce
Snehal Mane <snehal6mane@gmail.com>
2022-05-07 00:21:57 +0530

    Notice routes for faculty

---
+++ controller/faculty/notice.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+const Notice = require("../../models/Notice");
+const { idValidator } = require("../../utils/validationMiddleware");
+
+// @route   GET /api/student/notice/:_id
+// @desc    Get notice by id
+// @access  Private
+router.get("/:_id", idValidator, async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const noticeData = await Notice.findById(_id).sort({ createdAt: -1 }).select('title description branch year attachments addedBy createdAt updatedAt visible').populate('branch').populate('addedBy', '_id firstName lastName');
+    if (!noticeData || !noticeData.visible) return res.status(404).json({
+      error: 'Notice not found'
+    })
+
+    res.status(200).json(noticeData);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
+  }
+})
+
+// @route   GET /api/student/notice
+// @desc    Get notices relevant to student
+// @access  Private
+router.get("/", async (req, res) => {
+  try {
+    const noticeData = await Notice.find({
+      visible: true,
+    }).sort({ createdAt: -1 }).select('title description branch year attachments addedBy createdAt updatedAt').populate('branch').populate('addedBy', '_id firstName lastName');
+    res.status(200).json(noticeData)
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
+  }
+})
+
+module.exports = router;
--- routes/faculty.js
+++ routes/faculty.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const { facultyAuthMiddleware } = require('../utils/authMiddleware');
 const facultyRouter = require("../controller/faculty/quiz");
 const courseRouter = require("../controller/faculty/course");
+const noticeRouter = require("../controller/faculty/notice");
 
 router.use("/quiz", facultyAuthMiddleware, facultyRouter);
 router.use("/course", facultyAuthMiddleware, courseRouter);
+router.use("/notice", facultyAuthMiddleware, noticeRouter);
 
 module.exports = router;
